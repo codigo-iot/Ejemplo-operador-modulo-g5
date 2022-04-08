610555628eb7397cd961e6c30e124bb7d3c664d2
Hugo Escalpelo <hugoescalpelo@gmail.com>
2022-04-08 10:15:40 -0500

    Create Ejemplo_ESP32CAM_operadores.ino

---
+++ Ejemplo_ESP32CAM_operadores/Ejemplo_ESP32CAM_operadores.ino
@@ -0,0 +1,31 @@
+/*
+ * Ejemplo de operador módulo
+ * Por: Hugo Vargas
+ * Fecha: 8 de abril de 2022
+ * 
+ * Este es un programa para ejemplificar el uso básico del legunage C
+ * y el microcontrolador ESP32CAM.
+ * 
+ */
+
+// Bibliotecas
+
+// Constantes
+
+// Variables
+
+// Definición de objetos
+
+// Condiciones iniciales - Se ejecuta sólo una vez al energizar
+void setup() {// Inicio de void setup ()
+  // Aquí va tu código
+
+}// Fin de void setup
+
+// Cuerpo del programa - Se ejecuta constamente
+void loop() {// Inicio de void loop
+  // put your main code here, to run repeatedly:
+
+}// Fin de void loop
+
+// Funcioes del usuario
